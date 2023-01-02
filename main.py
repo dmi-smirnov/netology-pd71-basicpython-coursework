fc46c22f8d5e523059f492f5e87fdad04e522a22
Dmitriy Smirnov <git@d-sm.ru>
2023-01-02 15:16:45 +0300

    user input and choice max size photo added

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import sys
-sys.path.append('/tmp')
-from secret import vk_token,yadisk_token,gdrive_token
 import requests
 import time
 from pprint import pprint
@@ -33,12 +31,10 @@ class Yadisk(Cloud):
       f'HTTP request status code is {api_req_status_code}, '
       f'directory {dir_path} is already exists.')
       return 1
-
-    api_req_status_code = api_resp.status_code
     if api_req_status_code != 201:
       print(f'Error Yandex Disk API method {api_route}: '
       f'HTTP request status code is {api_req_status_code}')
-      return 1  
+      return 1
 
   def upload_from_url(self, url, file_path):
     api_route = '/v1/disk/resources/upload'
@@ -85,7 +81,7 @@ class VKuser():
       print(f'Error VK API method {api_method}: '
       f'HTTP request status code is {api_req_status_code}')
       return
-
+      
     if 'error' in api_resp.json():
       error_dict = api_resp.json()['error']
       print(f'Error VK API method {api_method}:\n'
@@ -173,36 +169,57 @@ class VKuser():
 
     return api_resp.json()['response']['items']
 
-  def backup_photos_to_cloud(self, api_token: str, cloud: Cloud,
-    album_photos_max_amt=5, only_profile_photos=False):
-    if only_profile_photos:
+  def backup_photos_to_cloud(self, api_token: str, cloud: Cloud):
+    all_albums_uploading_answer = not input('Загрузить все альбомы?\n'
+      'Enter - да, любой символ и Enter - только фотографии профиля: ')
+    if all_albums_uploading_answer:
+      print('Выбрана загрузка всех альбомов.', end='\n\n')
+    else:
+      print('Выбрана загрузка только фотографий профиля.', end='\n\n')
+    
+    print('Получение информации об альбомах пользователя... ', end='')
+    if all_albums_uploading_answer:
+      albums = self.get_albums(api_token)
+    else:
       profile_photos_album_id = '-6'
       albums = \
         self.get_albums(api_token, albums=[profile_photos_album_id])
-    else:
-      albums = self.get_albums(api_token)
     if albums is None:
       return
+    albums_amt = len(albums)
+    print(f'OK: альбомов {albums_amt}', end='\n\n')
     
+    album_photos_max_amt_input = \
+      input('Сколько сохранять фотографий из альбома?\n'
+      'Enter - 5 фотографий, число и Enter - задать количество: ')
+    if (isinstance(album_photos_max_amt_input, str) and 
+      album_photos_max_amt_input.isnumeric()):
+      album_photos_max_amt = int(album_photos_max_amt_input)
+    else:
+      album_photos_max_amt = 5
+    print(f'Выбрано сохранение не более {album_photos_max_amt} '
+      'фото с альбома.', end='\n\n')
+
     user_id = self.get_user_id(api_token)
     if user_id is None:
       return
 
+    
     backup_dir_path = (f'/vk_user_id_{user_id}'
       '_photos_backup')
+    print('Создание в облаке директории для пользователя... ', end='')
     if not cloud.create_dir(backup_dir_path) is None:
       return
+    print(f'OK: {backup_dir_path}', end='\n\n')
 
-    albums_amt = len(albums)
     album_count = 0
     for album in albums:
+      album_count += 1
       album_name = album["title"]
-      album_dir_path = (f'{backup_dir_path}/{album_name}')
-      if not cloud.create_dir(album_dir_path) is None:
-        return
-
-      album_id = album['id']
       album_size = album['size']
+      print(f'Альбом [{album_count}/{albums_amt}] {album_name}, {album_size} фото.')
+      album_dir_path = (f'{backup_dir_path}/{album_name}')
+
       if album_photos_max_amt > album_size:
         album_photos_amt = album_size
       else:
@@ -210,11 +227,19 @@ class VKuser():
       if album_photos_amt > 1000:
         album_photos_amt = 1000
       if album_photos_amt < album_size:
-        print(f'Album {album_name} contains {album_size} photos, '
-          f'but only {album_photos_amt} photos will be uploaded.')
-      album_photos = self.get_photos(api_token, album_id, album_photos_amt)
+        print(f'Будут загружены только {album_photos_amt} фото.')
+      
+      print('Создание в облаке директории для альбома... ', end='')
+      if not cloud.create_dir(album_dir_path) is None:
+        return
+      print(f'OK: {album_dir_path}')
+
+      print('Получение информации о фотографиях альбома... ', end='')
+      album_photos = \
+        self.get_photos(api_token, album['id'], album_photos_amt)
       if album_photos is None:
         return
+      print('OK.')
 
       # key is photo name, value is photos list
       repeated_names_photos_1 = dict()
@@ -255,38 +280,62 @@ class VKuser():
           uniq_name_photos[photo_name] = photo
 
       photo_count = 0
-      album_count += 1
-      print(f'Uploading album [{album_count}/{albums_amt}] {album_name}: '
-          f'photo [0/{len(uniq_name_photos)}]', end='')
+      print(f'Загрузка в облако '
+        f'фото [0/{len(uniq_name_photos)}]', end='')
+      
       for file_name,album_photo in uniq_name_photos.items():
-        max_size_photo = max(album_photo['sizes'],
-          key=lambda d: d['type'])
-        photo_url = max_size_photo['url']
+        photo_sizes_types = dict()
+        for photo_size in album_photo['sizes']:
+          photo_sizes_types[photo_size['type']] = photo_size['url']
+        for size_type in ('w', 'z', 'y', 'x', 'm', 's'):
+          if size_type in photo_sizes_types.keys():
+            max_size_photo_url = photo_sizes_types[size_type]
+            break
+        photo_url = max_size_photo_url
         file_path = f'{album_dir_path}/{file_name}'
-        # pprint(album_photo['sizes'])
-        # if 'width' in album_photo:
-        #   print(album_photo['width'], album_photo['height'])
-        # print(max_size_photo['width'], max_size_photo['height'], max_size_photo['type'])
         photo_count += 1
-        print(f'\rUploading album [{album_count}/{albums_amt}] {album_name}: '
-          f'photo [{photo_count}/{len(uniq_name_photos)}]', end='')
+        print(f'\rЗагрузка в облако '
+          f'фото [{photo_count}/{len(uniq_name_photos)}]...', end='')
         if not cloud.upload_from_url(photo_url, file_path) is None:
           return
-      print()
+      # print('\nЗагрузка альбома в облако завершена.', end='\n\n')
+      print('\rЗагрузка альбома в облако завершена.', end='\n\n')
 
-def main():
-  # Запрос на ввод данных, трубуемых по заданию
+def get_vk_user_id():
+  vk_user_id = input('Введите ID или видимое имя пользователя VK: ')
+  if vk_user_id.isnumeric():
+    if not input('Вы ввели ID? Enter - да, любой символ и Enter - нет: '):
+      vk_user_id = int(vk_user_id)
+      print('ID пользователя VK получен.', end='\n\n')
+      return vk_user_id
+  print('Видимое имя пользователя VK получено.', end='\n\n')
+  return vk_user_id
 
-  # vk_user_id = 'kuznya201386'
-  # vk_user_id = 764687792
-  vk_user_id = 'dmi.smirnov'
-  # vk_user_id = 'linka_evelin'
-  # vk_user_id = 'kitsuneri'
+def get_vk_token():
+  vk_token = None
+  while not vk_token:
+    vk_token = input('Введите ключ API VK: ')
+  print('Ключ API VK получен.', end='\n\n')
+  return vk_token
 
-  vk_user = VKuser(vk_user_id)
-  album_photos_max_amt = 100 
-  cloud = Yadisk(yadisk_token)
-  # vk_user.backup_photos_to_cloud(vk_token, cloud, album_photos_max_amt)
-  vk_user.backup_photos_to_cloud(vk_token, cloud)
+def get_cloud():
+  yadisk_or_gdrive = not input('Яндекс.Диск или Google Drive?\n'
+    'Enter - Яндекс.Диск, любой символ и Enter - Google Drive: ')
+  if yadisk_or_gdrive:
+    print('Выбрано облако Яндекс.Диск.', end='\n\n')
+  else:
+    print('Выбрано облако Google Drive.', end='\n\n')
+  cloud_token = None
+  while not cloud_token:
+    cloud_token = input('Введите ключ API выбранного облака: ')
+  if yadisk_or_gdrive:
+    cloud = Yadisk(cloud_token)
+  # else:
+  #   cloud = Gdrive(cloud_token)
+  print('Ключ API получен.', end='\n\n')
+  return cloud
 
-main()
+VKuser(get_vk_user_id()).backup_photos_to_cloud(
+  get_vk_token(),
+  get_cloud()
+)
